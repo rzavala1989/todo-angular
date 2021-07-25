957c05a4a000e32666d8c5c11401f41c7c359378
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-07-25 13:53:14 -0700

    commit before changing form toggle

--- ToDo/ToDo-angular/src/app/app.module.ts
+++ ToDo/ToDo-angular/src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TaskService } from './services/task.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TaskService } from "./services/task.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    NgbModule.forRoot()
-  ],
-  providers: [
-    TaskService
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, NgbModule.forRoot()],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
